f904147774836468a8c011b1596f85577220b140
Chris Simpkins <git.simpkins@gmail.com>
2022-04-05 12:29:12 -0400

    [size_csv.py] include nameID 5 version strings

--- size_csv.py
+++ size_csv.py
@@ -7,12 +7,16 @@ import csv
 import os
 import sys
 
+from pathlib import Path
+
+from fontTools.ttLib import TTFont
+
 
 def main(argv):
     font_dict = {}
     total_filesize = 0
     total_font_count = 0
-    fields = ["Font", "Size (B)"]
+    fields = ["Font", "Size (B)", "Version"]
     csv_data_list = []
 
     for fontpath in argv:
@@ -21,9 +25,30 @@ def main(argv):
         total_font_count += 1
         font_dict[os.path.basename(fontpath)] = size
 
+
     for key in sorted(font_dict):
-        print(f"{key} : {font_dict[key]}")
-        csv_data_list.append({fields[0]: key, fields[1]: font_dict[key]})
+        fontpath = Path(f"api_level/31/{key}")
+        if fontpath.suffix == ".ttc":
+            tt = TTFont(fontpath, fontNumber=0)
+        else:
+            tt = TTFont(fontpath)
+        
+        namerecord_list = tt["name"].names
+        # read in name records
+        for record in namerecord_list:
+            if record.nameID == 5:
+                version = record.toUnicode()
+                # remove any commas in the nameID 5 version string
+                # this is used in a comma delimited output format and we can't include them
+                version = version.replace(",", " ")
+                break
+
+        print(f"{key} : {font_dict[key]}, {version}")
+        csv_data_list.append({
+            fields[0]: key, 
+            fields[1]: font_dict[key], 
+            fields[2]: version
+        })
 
     print(f"\nTotal size: {total_filesize}")
     print(f"Total fonts: {total_font_count}")
